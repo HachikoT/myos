ba043ad5d38576a9a61cd79d89d6c955f51b9a44
rancheng <rancheng@baidu.com>
2022-03-25 15:43:58 +0800

    虚拟内存和置换区

--- kern/init/init.c
+++ kern/init/init.c
@@ -10,6 +10,11 @@ void kern_init(void) {
 
     pic_init()          // 初始化可编程中断控制器
     idt_init()          // 初始化中断描述符表
+
+    vmm_init();
+    ide_init();                 // init ide devices
+    swap_init();                // init swap
+
     intr_enable();      // 使能中断
 
     // 死循环
